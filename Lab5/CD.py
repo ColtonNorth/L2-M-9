82c31bc831518c2f59a04d7916161286fc8c13bd
Justice Ayela <justiceayela@cmail.carleton.ca>
2020-10-19 15:10:54 -0400

    update code with D identifier added

--- Lab5/CD.py
+++ Lab5/CD.py
@@ -1,7 +1,9 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
+import D from *
 
 def function_CD():
+    function_D()  #Line added by team member D
     print ("CD")
     
 
---
+++ Lab5/D.py
@@ -0,0 +1,4 @@
+# SYSC3010 Lab 5 - D.py
+#     Function to print D identifier
+def function_D():
+    print("D")
